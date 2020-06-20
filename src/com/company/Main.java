f3bebdb2e138033eeae4e7c90777c5a350c04ec0
Milind Sahoo <milind.sahoo@mobiliya.com>
2020-06-20 20:53:59 +0530

    missing files added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,17 @@
 package com.company;
 
+import com.company.Bridge.BridgeExecutor;
 import com.company.Flyweight.FlyweightExecutor;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        FlyweightExecutor flyweightExecutor = new FlyweightExecutor();
-        flyweightExecutor.execute();
+        // write your code here
+//        FlyweightExecutor flyweightExecutor = new FlyweightExecutor();
+//        flyweightExecutor.execute();
+
+        BridgeExecutor bridgeExecutor = new BridgeExecutor();
+        bridgeExecutor.execute();
+
     }
 }
