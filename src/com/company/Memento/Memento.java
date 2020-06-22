1ed97a26023650f3e8e93536972bc6a55b591442
Milind Sahoo <milind.sahoo@mobiliya.com>
2020-06-22 10:01:16 +0530

    memento pattern

---
+++ src/com/company/Memento/Memento.java
@@ -0,0 +1,14 @@
+package com.company.Memento;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
